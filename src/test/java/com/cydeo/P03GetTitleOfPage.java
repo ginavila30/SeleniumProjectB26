029aa76b8be30b963a7fe24606e8a170048bbbe2
Gina <ginavila30@icloud.com>
2022-05-07 21:56:30 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/P03GetTitleOfPage.java
@@ -0,0 +1,28 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P03GetTitleOfPage {
+    public static void main(String[] args) {
+        //set up chrome and create web driver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://cydeo.com"); // navigate to google
+
+        String title= driver.getTitle();
+        System.out.println("Title of the page: "+title);
+
+        if(title.equals("Cydeo")){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test failed");
+        }
+
+        driver.quit();
+
+
+    }
+}
---
+++ src/test/java/com/cydeo/SeleniumTest.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        // 1- Setting up web driver manager-> chrome driver.
+       WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\majid\\OneDrive\\Desktop\\chromedriver.exe");
+        // 2- Create instance of the chrome driver
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.google.com");
+    }
+}
+
+
