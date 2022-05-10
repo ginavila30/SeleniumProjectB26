0e5e30eda61616d08b8c259b91e46611845df798
Gina <ginavila30@icloud.com>
2022-05-10 12:25:05 -0400

    Work done 5/8/2022

---
+++ src/test/java/com/cydeo/test/day1_selenium_intro/Task01_Yahoo_Title_Verification.java
@@ -0,0 +1,29 @@
+package com.cydeo.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task01_Yahoo_Title_Verification {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.yahoo.com");
+
+        String actualTitle=driver.getTitle();// title comes from selenium by the driver method
+        String expectedTitle="Yahoo";// title comes from the requirements
+
+        if(actualTitle.contains(expectedTitle)){
+            System.out.println("Title verification passed");
+        }else{
+            System.out.println("Title verification failed");
+        }
+
+        driver.quit();
+
+
+    }
+}
--- src/test/java/com/cydeo/test/short_video_practice/P02CloseAndQuitBrowser.java
+++ src/test/java/com/cydeo/test/short_video_practice/P02CloseAndQuitBrowser.java
@@ -1,4 +1,4 @@
-package com.cydeo;
+package com.cydeo.test.short_video_practice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
