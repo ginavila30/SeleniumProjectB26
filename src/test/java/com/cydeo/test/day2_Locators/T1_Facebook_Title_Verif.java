6059097db746a7d9d029b5cbfcdb60ee2fb1a3db
Gina <ginavila30@icloud.com>
2022-05-11 22:04:10 -0400

    Work done 5/11/2022

--- src/test/java/com/cydeo/test/day2_Locators/T1_Facebook_Title_Verif.java
+++ src/test/java/com/cydeo/test/day2_Locators/T1_Facebook_Title_Verif.java
@@ -1,10 +1,10 @@
-package com.cydeo.test.warm_up_task;
+package com.cydeo.test.day2_Locators;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-public class Task01_Facebook_Title_Verif {
+/**/
+public class T1_Facebook_Title_Verif {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
