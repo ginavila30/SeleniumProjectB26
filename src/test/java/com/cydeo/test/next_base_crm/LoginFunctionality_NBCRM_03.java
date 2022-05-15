4765df47e3c654f399f0582a16ec20f57e0c8345
Gina <ginavila30@icloud.com>
2022-05-14 23:41:58 -0400

    Work done on 5/14/2022

---
+++ src/test/java/com/cydeo/test/next_base_crm/LoginFunctionality_NBCRM_03.java
@@ -0,0 +1,36 @@
+package com.cydeo.test.next_base_crm;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginFunctionality_NBCRM_03 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://login2.nextbasecrm.com/");
+        WebElement userNameInputBox= driver.findElement(By.name("USER_LOGIN"));
+        userNameInputBox.sendKeys("hr22@cybertekschool.com");
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.getMessage();
+        }
+        WebElement passwordInputBox= driver.findElement(By.name("USER_PASSWORD"));
+        passwordInputBox.sendKeys("wrong");
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+        WebElement invalidMessage= driver.findElement(By.className("errortext"));
+
+        String actualInvalidText= invalidMessage.getText();
+        String expectedInvalidTest="Incorrect login or password";
+        if(actualInvalidText.equals(expectedInvalidTest)){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+    }
+}
