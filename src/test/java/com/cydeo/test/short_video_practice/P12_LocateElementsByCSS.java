636bc3056d35a6fa54ca53dc15dca201ccb39ed9
Gina <ginavila30@icloud.com>
2022-05-19 01:02:11 -0400

    Work done 5/18/2022

---
+++ src/test/java/com/cydeo/test/short_video_practice/P12_LocateElementsByCSS.java
@@ -0,0 +1,32 @@
+package com.cydeo.test.short_video_practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P12_LocateElementsByCSS {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com/");
+//search for cydeo
+        WebElement searchBox = driver.findElement(By.cssSelector("input[name='q']"));
+        searchBox.sendKeys("cydeo" + Keys.ENTER);
+//Verify cydeo link text
+        WebElement cydeoText = driver.findElement(By.cssSelector("a[href='https://cydeo.com/']>h3"));
+
+        if (cydeoText.getText().equals("Cydeo")) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+
+        driver.quit();
+
+    }
+}
